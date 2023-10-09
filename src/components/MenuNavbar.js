8a18ab30f4e18b5f8c093e3becb74b5b199807ce
Ghassen Lahdhiri <75281993+Ghassenl@users.noreply.github.com>
2023-10-09 14:53:11 +0200

    fixed the pages and made the navbar a component

---
+++ src/components/MenuNavbar.js
@@ -0,0 +1,72 @@
+// MenuComponent.js
+import React from "react";
+import { Menu, Avatar, Button } from "antd";
+import {
+  HomeTwoTone,
+  BulbTwoTone,
+  BankTwoTone,
+  AlertTwoTone,
+} from "@ant-design/icons";
+
+const url = "https://cdn-icons-png.flaticon.com/512/9805/9805356.png";
+
+const MenuComponent = ({ current, onClick }) => {
+  const items = [
+    {
+      label: (
+        <div className="logo">
+          <Avatar
+            src={url}
+            size={{
+              xs: 24,
+              sm: 24,
+              md: 30,
+              lg: 30,
+              xl: 40,
+              xxl: 50,
+            }}
+          />
+          <span style={{ color: "#75BF7A" }}>CITYMAPER</span>
+        </div>
+      ),
+      key: "Logo",
+      disabled: true,
+    },
+    {
+      label: "HOME",
+      key: "home",
+      icon: <HomeTwoTone style={{ fontSize: "24px" }} />,
+    },
+    {
+      label: "HOW IT WORKS",
+      key: "how",
+      icon: <BulbTwoTone style={{ fontSize: "24px" }} />,
+    },
+    {
+      label: "MONUMENTS",
+      key: "monuments",
+      icon: <BankTwoTone style={{ fontSize: "24px" }} />,
+    },
+    {
+      label: (
+        <Button
+          type="primary"
+          icon={
+            <AlertTwoTone twoToneColor="white" style={{ fontSize: "24px" }} />
+          }
+          size="large"
+          style={{ backgroundColor: "#75BF7A", color: "white" }}
+        >
+          HELP CENTER
+        </Button>
+      ),
+      key: "Help",
+    },
+  ];
+
+  return (
+    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+  );
+};
+
+export default MenuComponent;
---
+++ src/pages/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div>
+      <h1>ttttttttttttttttttttttttttttttttttest</h1>
+    </div>
+  );
+};
+
+export { Map };
