587ff45071e7b6e3098a46d08aed6de232e38803
Ghassen Lahdhiri <75281993+Ghassenl@users.noreply.github.com>
2023-10-10 15:58:01 +0200

    Added the map page with  the monuments board on the side of the map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,9 +1,96 @@
-import React from "react";
-
+import React, { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import { Button, Drawer, Card } from "antd";
+import "./Map.css";
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const { Meta } = Card;
 const Map = () => {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  const titleStyle = {
+    color: "#75BF7A",
+    fontSize:20,
+    fontWeight:600,
+  };
+  const descriptionStyle = {
+    fontStyle: "italic",
+    fontWeight: 700,
+    fontSize:14,
+    color: "blue",
+    textTransform: "uppercase",
+  };
   return (
     <div>
-      <h1>ttttttttttttttttttttttttttttttttttest</h1>
+      <div className="MapContainer">
+        <div className="GoogleMap" style={{ height: "93vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "" }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+            <AnyReactComponent lat={48.8566} lng={2.3522} text="Paris" />
+          </GoogleMapReact>
+        </div>
+        <Button className="BottomLeftButton" onClick={showDrawer}>
+          Your Button
+        </Button>
+      </div>
+      <div className="MonumentsDrawer">
+        <Drawer
+          title="Your Monuments Order"
+          placement="right"
+          onClose={onClose}
+          open={open}
+        >
+          <p>
+            <Card
+              style={{
+                width: 300,
+                marginBottom: 50,
+              }}
+              cover={<img alt="eiffel tower" src="/eiffel.png" />}
+            >
+              <Meta
+                title={<span style={titleStyle}>EIFFEL TOWER</span>}
+                description={
+                  <span style={descriptionStyle}>
+                    The Popular EIFFEL TOWER built by Gustave Eiffel
+                  </span>
+                }
+              />
+            </Card>
+            <Card
+              style={{
+                width: 300,
+                marginBottom: 50,
+              }}
+              cover={<img alt="Louvre" src="/louvre.jpg" />}
+            >
+              <Meta
+                title={<span style={titleStyle}>LOUVRE</span>}
+                description={
+                  <span style={descriptionStyle}>
+                    The Louvre, or the Louvre Museum, is a national art museum
+                    in Paris, France.
+                  </span>
+                }
+              />
+            </Card>
+          </p>
+        </Drawer>
+      </div>
     </div>
   );
 };
